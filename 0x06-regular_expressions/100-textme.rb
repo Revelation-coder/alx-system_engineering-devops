3ff9956d3c78f8314bd8abbb1358d4a4a3bb11dc
Revelation-coder <captainrevelation@gmail.com>
2023-05-04 06:25:39 -0700

    advanced

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,14 +1,4 @@
 #!/usr/bin/env ruby
-# regex built for Oniguruma library
 
-# pattern to extract sender, receiver, and flags
-pattern='s/.*from:(\S+).+\[to:(\S+)\].+\[flags:(\S+)\].*/\1,\2,\3/'
-
-# read the log file line by line and extract the necessary information
-while read -r line; do
-  # apply the pattern and print the result
-  if [[ $line =~ $pattern ]]; then
-    echo "${BASH_REMATCH[1]},${BASH_REMATCH[2]},${BASH_REMATCH[3]}"
-  fi
-done < "$1"
-
+string = ARGV[0]
+puts string.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",") 
