b09e0b5d6de0962befd9679b84f06cbb1fe762f5
Revelation-coder <captainrevelation@gmail.com>
2023-06-28 05:01:01 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,5 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
+# Install Nginx web server (w/ Puppet)
+exec { 'server configuration':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
-
-# Configure Nginx server
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-    server {
-      listen 80;
-      location / {
-        return 301 http://$::ipaddress/;
-      }
-      location = / {
-        echo 'Hello World!';
-      }
-    }
-  ",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable Nginx default site
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  require   => Package['nginx'],
-}
-
